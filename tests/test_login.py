d3c43b6dea085404a69b93949121896e0a1dbdbe
Yaashick Sriram <yaashshiva1012@gmail.com>
2026-09-08 23:46:10 +0530

    Fix for Intentional Failure

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,5 +2,5 @@ def test_login():
     userName = 'Yaashick Sriram'
     passWord = 'admin_123'
 
-    assert userName == 'Yaashick'
+    assert userName == 'Yaashick Sriram'
     assert passWord == 'admin_123'
